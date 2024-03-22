0424cc74ee623e7eb01663fa2b4c4dec6ed4508e
AmanDubeyy <dubey7920@gmail.com>
2024-03-23 01:28:41 +0530

    recent activity

--- src/app/recents/Recents.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Recents() {
-  return (
-    <div>Recents</div>
-  )
-}
-
-export default Recents
---
+++ src/app/recents/page.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import RecentListItem from "./RecentListItem";
+
+function page() {
+  const recents = [
+    { description: "you added 5 rs" },
+    { description: "you added 100 rs" },
+    { description: "you added 100 rs" },
+    { description: "you added 100 rs" },
+    { description: "you added 100 rs" },
+    { description: "you added 100 rs" },
+    { description: "you added 100 rs" },
+    { description: "you added 100 rs" },
+  ];
+
+  return (
+    <div className="flex flex-col">
+      <div className="font-sans font-extrabold text-4xl text-center sticky top-0 z-5 bg-black p-12">
+        Recent Activity
+      </div>
+      <div className="">
+        {recents.map((item, index) => (
+          <ul key={index}>
+            <RecentListItem item={item} />
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default page;
