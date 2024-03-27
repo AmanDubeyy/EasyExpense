604d10a311d301a3ae3e6a7dd6cd9f2aa1214fef
amandubeyy <dubey7920@gmail.com>
2024-03-27 14:47:10 +0530

    readibility improved:

--- src/lib/connectDB.js
+++ src/lib/connectDB.js
@@ -2,11 +2,13 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
+
         await mongoose.connect(MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-        });
+        }); 
 
+        console.log('connection to mongoDB successfull');
     } catch (e) {
         console.log("Failed to connect mongo" + e.message);
     }
