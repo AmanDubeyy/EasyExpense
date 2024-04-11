9f8aa41e14f5dbc4d72d8e7a1ab84b998ddcd0b8
AmanDubeyy <dubey7920@gmail.com>
2024-04-11 20:03:41 +0530

    group info

--- src/app/groups/[...group_name]/page.jsx
+++ src/app/groups/[...group_name]/page.jsx
@@ -1,16 +1,29 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
 function page() {
+  const [groupData, setGroupData] = useState({
+    _id: "abc",
+    group_name: "Teen Patti",
+    group_id: "abc",
+  });
 
-    const groupData = {
-        _id : 'abc',
-        group_name : 'Teen Patti' 
-    }
+  const [recentExpenses, setRecentExpenses] = useState([]);
 
-    return (
-        <div>
-        </div>
-    )
+  const Menu = ["Recent Expenses", "Balances", "Total"];
+
+  return (
+    <div className="flex flex-col bg-my-light-blue items-center">
+      <h2 className="p-2 text-3xl">{groupData?.group_name}</h2>
+      <div className="flex flex-row items-center p-4">
+        {Object.keys(Menu).map((key) => (
+          <p key={key} className="p-4 text-xl">
+            {Menu[key]}
+          </p>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default page
+export default page;
